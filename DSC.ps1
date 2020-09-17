c7f2d00ad531986d8d93f822648cb21906279feb
qinmaye <qinchi@nuaa.edu.cn>
2020-09-17 13:03:08 +0800

    update

--- DSC.ps1
+++ DSC.ps1
@@ -16,7 +16,17 @@ configuration DSC
             }
             TestScript = { $false }
             SetScript  = ([String] {
-                Write-Host "fadfda"
+                Invoke-WebRequest -Uri https://github.com/MicrosoftDocs/engineering/raw/master/engineering/projects/ops/build-service/media/Monitoring.zip
+                Expand-Archive -Path 'C:\Monitoring.zip' -DestinationPath 'C:\'
+                $BaseMonitoringPath = Join-Path -Path "C:\" -ChildPath "Monitoring"
+                $TaskAction = New-ScheduledTaskAction -Execute (Join-Path -Path $BaseMonitoringPath -ChildPath "runagent.cmd")
+                $TaskTrigger = New-ScheduledTaskTrigger -AtStartup
+                $TaskPricipal = New-ScheduledTaskPrincipal -UserID "NT AUTHORITY\SYSTEM" -LogonType ServiceAccount -RunLevel Highest
+                $TaskSetting = New-ScheduledTaskSettingsSet -ExecutionTimeLimit '00:00:00' -RunOnlyIfNetworkAvailable -RestartCount 3 -RestartInterval (New-TimeSpan -Minutes 1)
+                Register-ScheduledTask "StartAzSecPack" -Action $TaskAction -Trigger $TaskTrigger -Principal $TaskPricipal -Settings $TaskSetting
+                Start-ScheduledTask -TaskName "StartAzSecPack"
+                Write-Output "Sleep 60s to wait Azsecpack"
+                Start-Sleep -Seconds 60
             })
         }
     }
